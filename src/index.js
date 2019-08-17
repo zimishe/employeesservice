b57166f110337b27e6381d756dca9ffde775be1f
Yevhen Shuliakivskiy <zimishe@gmail.com>
2019-08-17 13:03:17 +0300

    fixed accepted headers

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	next();
 });
 
