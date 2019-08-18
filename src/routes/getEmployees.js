e68393c4e40db8ae938202fd0cd3ce19449c570b
Yevhen Shuliakivskiy <zimishe@gmail.com>
2019-08-18 17:48:13 +0300

    removed check for token for get request

--- src/routes/getEmployees.js
+++ src/routes/getEmployees.js
@@ -1,26 +1,20 @@
 const { dbName } = require('../config/db');
 const {	TARGET_COLLECTION, ENDPOINT_URL } = require('../constants');
-const { checkToken } = require('../utils/authorization');
 
 module.exports = (app, client) => {
 	app.get(ENDPOINT_URL, async (req, res) => {
-		const token = req.get('Authorization');
 		const db = client.db(dbName);
 
 		try {
-			const { status } = await checkToken(token);
-
-			if (status === 200) {
-				db.collection(TARGET_COLLECTION)
-					.find()
-					.toArray((error, result) => {
-						if (error) {
-							res.send({ error });
-						} else {
-							Boolean(result) ? res.send(result) : res.sendStatus(404)
-						}
-				});
-			}
+			db.collection(TARGET_COLLECTION)
+				.find()
+				.toArray((error, result) => {
+					if (error) {
+						res.send({ error });
+					} else {
+						Boolean(result) ? res.send(result) : res.sendStatus(404)
+					}
+			});
 		} catch (error) {
 			const status = error.response && error.response.status
 			res.sendStatus(status);
